20e5a3912a0ffa8e07757953c0e273ee63d7e72c
shafran <shafran.andriy@gmail.com>
2025-01-21 22:44:04 +0200

    Created tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from datetime import date
+from unittest import mock
+
+import pytest
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize("current_product, expected_output",
+                         [
+                             pytest.param([
+                                 {"name": "salmon",
+                                  "expiration_date": date(2025, 1, 25),
+                                  "price": 600},
+                                 {"name": "chicken",
+                                  "expiration_date": date(2025, 1, 30),
+                                  "price": 120}],
+                                 [],
+                                 id="Expiration day today not outdated"),
+                             pytest.param([
+                                 {"name": "salmon",
+                                  "expiration_date": date(2025, 1, 19),
+                                  "price": 600}],
+                                 ["salmon"],
+                                 id="Expiration day yesterday outdated")
+                         ])
+def test_outdated_products(current_product: list[dict],
+                           expected_output: list[str]):
+    with mock.patch("datetime.date.today") as mock_date:
+        mock_date.today.return_value = date(2025, 1, 20)
+        assert outdated_products(current_product) == expected_output
